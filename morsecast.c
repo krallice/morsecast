7fdd1fee791e7ed7a1f18e86e6104c497ae97b2d
Krallice <sroz9960@gmail.com>
2015-10-20 18:31:21 +1100

    Create socket, enable UDP and enable broadcast. Zero out structure

--- morsecast.c
+++ morsecast.c
@@ -13,6 +13,9 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+#define DESTINATION "255.255.255.255"
+#define DESTPORT 7474
+	
 // Our Bomb-out Function:
 void die(char *s) {
 	perror(s);
@@ -21,6 +24,37 @@ void die(char *s) {
 
 int main(void) {
 
+	// Useless preamble:
 	printf("In trouble?? OK, Let's get you some help!\n");
+	
+	// Create an address structure in memory for the connection
+	// Per man pages, this struct looks like this:
+	/*
+		struct sockaddr_in {
+			sa_family_t    sin_family; address family: AF_INET 
+			in_port_t      sin_port;   port in network byte order 
+			struct in_addr sin_addr;   internet address 
+		};
+		Internet address: 
+		struct in_addr {
+			uint32_t       s_addr;     address in network byte order 
+		};
+	*/
+	// Dial in one of these structures for our destination socket:
+	struct sockaddr_in sockAddr;
+	
+	// Create a socket filedescriptor for an INET/Datagram through UDP socket:
+	int sockfd, i, sockLen=sizeof(sockAddr);
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+		die("socket");
+	}
 
+	// Enable Broadcast on the socket:
+	int broadcastOn = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcastOn, sizeof(broadcastOn)) == -1) {
+		die("setsockopt");
+	}
+	
+	// Zero out the socket:
+	memset((char *) &sockAddr, 0, sockLen);
 }
